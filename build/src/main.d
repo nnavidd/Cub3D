fd44380c64f1db62a6d8ee6b3e0f1c3fda1c04df
nnavidd <navid.nabaeei@gmail.com>
2024-02-21 07:11:55 -0800

    commenting printf func in the extract_rgb fucn

--- build/src/main.d
+++
@@ -1,6 +0,0 @@
-build/src/main.o: src/main.c src/../include/cub_3d.h \
-  include/../libs/MLX42/include/MLX42/MLX42.h \
-  include/../libs/libft/fts.h libs/libft/./libft/libft.h \
-  libs/libft/./gnl/get_next_line.h libs/libft/./ft_printf/ft_printf.h \
-  libs/libft/./ft_printf/../libft/libft.h include/../include/utils.h \
-  include/../include/parser.h include/../include/cub_3d.h
